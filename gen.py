a0e9b9900335cc02c0890d9c6fabf088ba733d45
phonphey <phonphey@o2.pl>
2017-02-17 20:07:29 +0300

    _

--- gen.py
+++ gen.py
@@ -6,50 +6,31 @@
 
 import random
 
-COLL_OBS = ["*", "-", "_", "A",
-            "B", "C"]  # Коллекция припятствий
 
 class Gen():
     ''' Класс генераторов'''
 
     def rand_map_gen(self):
-        ''' Функция-генератор случайной, временной карты '''
+        
         gen_map = list()
-        unluky_obs = 0
-        for i in range(0, 100, 1):  # Генератор карты
-            tmp_str = ""
-            tumb = True
+
+        for i in range(0, 150, 1):
+
             k = 0
-            while tumb:  # Генератор строки в карте
+            tumb = True
+            tmp_str = ""
 
-                if k == 0:  # Проверяем нужна ли стенка
-                    tmp_str += "| "
+            while tumb:
+                
+                if k == 0:
+                    tmp_str += "|"
 
-                tmp_unluky_obs = unluky_obs
-                # Вероятность появления препятствия
-                unluky_obs = random.randint(1, 45)
-                # Выбор случайного препятствия
-                rand_obs = random.choice(COLL_OBS)
+                lucky = random.randint(1, 100)
 
-                # Проверка вероятности и контроль длинны строки
-                if unluky_obs > 17 and tmp_unluky_obs < 17 and len(tmp_str + rand_obs) <= 12:
-                    tmp_str += random.choice(COLL_OBS)
+                if lucky <= 50:
 
-                elif len(tmp_str + rand_obs) <= 12:
-                    tmp_str += " "
-
-                elif len(tmp_str + rand_obs) >= 12:
-                    tmp_str = tmp_str[:10]
-                    tumb = False
-                    tmp_str += " |"
-
-                k += 1
-
-            gen_map.append(tmp_str)
-        print(gen_map)
-        return gen_map
+                
 
 if __name__ == "__main__":
     TMP_CLASS = Gen()
     TMP = TMP_CLASS.rand_map_gen()
-    print(TMP, len(TMP[0]), len(TMP[55]))
