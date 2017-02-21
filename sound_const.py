e8dec65cd545ee4d0afe8915aeb443cb968777d9
Unknown <kdv200330@bk.ru>
2017-02-21 22:26:45 +0300

    Music.py

---
+++ sound_const.py
@@ -0,0 +1,12 @@
+''' Header file with sound  const'''
+
+SOUND1 = "res/sound/den.ogg"
+SOUND2 = "res/sound/den2.ogg"
+SOUND3 = "res/sound/den3.ogg"
+SOUND4 = "res/sound/den4.ogg"
+SOUND5 = "res/sound/den5.ogg"
+SOUND6 = "res/sound/den6.ogg"
+SOUND7 = "res/sound/den7.ogg"
+SOUND8 = "res/sound/den8.ogg"
+SOUND9 = "res/sound/den9.ogg"
+SOUND10 = "res/sound/den10.ogg"
